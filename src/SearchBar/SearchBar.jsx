0c98e390a7bf0138a0a012bcf627b95794ecc523
Brendan Hurt <brendan.q.hurt@gmail.com>
2021-04-10 14:48:28 -0700

    added search bar text field

--- src/SearchBar/SearchBar.jsx
+++ src/SearchBar/SearchBar.jsx
@@ -6,16 +6,26 @@ import './SearchBar.css';
 */
 
 export default class SearchBar extends Component {
-    constructor(props) {
-        super(props);
+    constructor() {
+        super();
         this.state = {
-            input: "",
+            search: 'Type Here',
+            inputVal: '',
         };
     }
-
+    updateSearch(event) {
+        console.log(event.target.value);
+        this.setState({search: event.target.value});
+    }
     render() {
-        return (<div><h1>This is the search bar!</h1>
-
-        </div>);
+        return(
+            <div>
+                <h1>This is the search bar!</h1>
+                <input type="text" 
+                    value={this.state.search}
+                    onChange={this.updateSearch.bind(this)}
+                />
+            </div>
+        );
     }
 }
