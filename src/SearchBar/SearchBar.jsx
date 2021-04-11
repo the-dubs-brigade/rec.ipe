0083038a0c442685441148a59b753b7799a22ac3
Brendan Hurt <brendan.q.hurt@gmail.com>
2021-04-10 19:42:47 -0700

    Search bar now creates an array on ingredient strings

--- src/SearchBar/SearchBar.jsx
+++ src/SearchBar/SearchBar.jsx
@@ -9,13 +9,36 @@ export default class SearchBar extends Component {
     constructor() {
         super();
         this.state = {
-            search: 'Type Here',
-            inputVal: '',
+            search: 'Search for ingredients',   //text in the search bar
+            ingredients: '',                    //will hold the array of ingredients
         };
     }
+    //detecting enter inputs
+    handleKeyPress(event) {
+        if (event.key === 'Enter') {
+            
+            //create the array of ingredients
+            this.setState({ingredients: this.state.search.split(',')});
+
+            //testing the ingredients array
+            //setTimeout(()=> {console.log(this.state.ingredients);}, 1);
+        }
+    }
+    //handles typing into the search bar
     updateSearch(event) {
-        console.log(event.target.value);
-        this.setState({search: event.target.value});
+
+        var str = event.target.value;
+        var oldText = str.substring(0, str.length - 1);
+
+        //if the default text is there, remove it
+        //otherwise, just add the new character to the end
+        if (oldText === 'Search for ingredients') {
+            this.setState(
+                {search: str.substring(str.length - 1, str.length)}
+            );
+        } else {
+            this.setState({search: str});
+        }
     }
     render() {
         return(
@@ -23,6 +46,7 @@ export default class SearchBar extends Component {
                 <h1>This is the search bar!</h1>
                 <input type="text" 
                     value={this.state.search}
+                    onKeyPress={this.handleKeyPress.bind(this)}
                     onChange={this.updateSearch.bind(this)}
                 />
             </div>
